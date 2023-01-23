88c1875a0b0a9d1dfd8d4189629e0c16c96d77a6
rejaul-karim10 <rejalkarim66666@gmail.com>
2023-01-24 04:52:40 +0800

    added settings icon to the home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import React from "react";
+import {FiSettings} from "react-icons/fi"
+import { BrowserRouter } from "react-router-dom";
 
-import './app.css'
+import "./app.css";
 
 const App = () => {
   return (
-    
-      <h1 className='text-3xl underline'>This is app</h1>
-    
+    <div>
+      <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{zIndex: "1000"}}>
+          <TooltipComponent content="Settings" position="top">
+            <button type="button" className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white" style={{background: "blue", borderRadius: "50%"}}>
+              <FiSettings/>
+            </button>
+          </TooltipComponent>
+        </div>
+      </div>
+      </BrowserRouter>
+    </div>
   )
-}
+};
 
-export default App
+export default App;
